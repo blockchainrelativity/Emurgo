b40c0de1fdd975d26ec6e13c4352d97dc799028d
BlockChain Relativity <blockchainrelativity@protonmail.com>
2022-05-02 10:40:19 -0700

    Updates to assignments solutions

--- Assignments/2/assigment02.hs
+++ Assignments/2/assigment02.hs
@@ -1,27 +1,14 @@
-{- Absolute value of any number
-    Create a function named Absolute that takes a value and return the absolute (the number wihtout the sign) 
-    Examples:   Absolute 10  = 10
-                Absolute -10 = 10
-                Absolute -11.11 = 11.11
-    
-    Create function using if-then, guards and case
-    
-absolute :: Double -> Double
-absolute n = do
-    if n <= 0  
-      then n == (-n)
-    if n >= 0
-      then n == n
+module Assigment where
 
-abslt :: (Num a) => a -> a
-abslt x = absolute x
-absolute (-10) = 10
-absolute (-11.11) = 11.11
-absolute x = 0
-  | otherwise x = True
--}
-
-{- correct answers here, but both work:
+  {- Absolute value of any number
+     Create a function named Absolute that takes a value and return the absolute (the number wihtout the sign) 
+     Examples:   Absolute 10  = 10
+                 Absolute -10 = 10
+                 Absolute -11.11 = 11.11
+     
+     Create function using if-then, guards and case
+  -}               
+  
   absoluteI :: Float -> Float
   absoluteI n = if n >= 0 then n else n * (-1)
 
@@ -40,30 +27,6 @@ absolute x = 0
   absInf n  
    | n < 0     = n * (-1)
    | otherwise = n
--}
-
-absolute :: Double -> Double
-absolute n
-  | n < 0       = (-n)
-  | n >= 0      = n
-
--- absolutedo :: Int -> Int
--- absolutedo n = do
---     if n == 10 || (-10) 
---       then n == 10
---     if n == (-11.11)
---       then n == 11.11
---     else n == 0
-
--- Missing the Eq a, add Eq a to the context of the type signature
--- absolutecase :: Double -> a
--- absolutecase x = 
---     case x of
---         10       -> 10
---         (-10)    -> 10
---         (-11.11) -> 11.11
---         _        -> 0 
-
 
   -- Bonus:  Try to make any of the functions polymorphic, find any problems? can you explain why? Whats the solution?
 
@@ -72,31 +35,6 @@ absolute n
      F for converting from Celsis to Farenheit
      The conversion basic formula   (F - 32) x 5/9 = C
   -}
-
---Scale and Temp conversion steps
--- convert2F :: (Fractional a) => a -> a
-convert2F :: Double -> Double
-convert2F temp =
-  ((/) temp (0.555555)) + 32
-
--- convert2C :: (Fractional a) => a -> a
-convert2C :: Double -> Double
-convert2C temp =
-  (temp - 32) * (0.555555)
-
--- convertTemp :: (Fractional a) => a -> Char -> a
-convertTemp :: Double -> Char -> Double
-convertTemp temp scale
-  | scale == 'C' = convert2F temp
-  | scale == 'F' = convert2C temp
-          
-
--- comfortZone :: (Fractional a) => a -> Char -> String
-comfortZone :: Double -> Char -> String
-comfortZone convertTemp scale
-  | convertTemp <= 18.00 && scale == 'C' || scale == 'F' && convertTemp <= 64.4 = "It's too cold!" 
-  | scale == 'C' && convertTemp >= 24 || scale == 'F' && convertTemp >= 75.2 = "It's too hot"
-  | otherwise = "Its comfortable"
 
   tempConverter :: Char -> Float -> Float
   tempConverter tu tm 
@@ -108,7 +46,7 @@ comfortZone convertTemp scale
      Create a function that answers "It's too cold!" if temperature below the range of been confortable, 
      or "It's too hot" if its above and "Its comfortable" if its in range.  
      As in previous assigment you should identify input with C for Celsius or F for Farenheiht
-
+  -}
 
   comfortable :: Char -> Float -> String
   comfortable 'C' tm
@@ -120,4 +58,5 @@ comfortZone convertTemp scale
    | tm > 75.2     = "It's too hot"
    | otherwise     = "Its comfortable" 
   comfortable _ _ = "You must choose the proper temperature unit (C or F)"
-  -}
+  
+  
